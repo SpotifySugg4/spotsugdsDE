8551a34cf8dda7c468a2a0b9e5528af9d523f036
Brooke Drake <iambrookedrake@gmail.com>
2020-10-21 21:32:37 -0600

    NearestNeighborsModel

--- sugg/models.py
+++ sugg/models.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-from sklearn.neighbors import KDTree
+from sklearn.neighbors import NearestNeighbors
 import joblib
 import pickle
 import spotipy
@@ -26,19 +26,20 @@ spotifyModel = pickle.load(open('./spotifyModel.pkl', 'rb'))
 def askTheModel(tempSongID='1Cj2vqUwlJVG27gJrun92y'):
   # some variables I need set up
   features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness',
-              'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
-              'time_signature']
+            'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
+            'time_signature']
   temp = pd.DataFrame
-  #print(temp)
+  #tempSongID = '6meIeOX3DHdaCnaNw67abE'
   similarSongList = []
-  # download the song data from spotify and scale it
   x = temp.from_dict(spotify.audio_features(tempSongID))[features]
-  x_scaled = songScaler.transform(x)
-  inputSongScaled = pd.DataFrame(x_scaled)
-  # ask the model
-  suggestedSongs = spotifyModel.query(np.array(inputSongScaled).reshape(1, -1), k=21)
+  inputSongScaled = songScaler.transform(x)
+  #suggestedSongs = spotifyModel.kneighbors(np.array(inputSongScaled).reshape(1, -1), n_neighbors=21)
+  suggestedSongs = spotifyModel.kneighbors(np.array(inputSongScaled).reshape(1, -1), n_neighbors=21, return_distance=False) #added , return_distance=False
+  #  return song IDs
+  
   #  convert the answers to song IDs
-  for song in suggestedSongs[1][0]:
+  #for song in suggestedSongs[1][0]:
+  for song in suggestedSongs[0]:
     if (songID['id'][song]) != tempSongID:
       similarSongList = similarSongList + [(songID['id'][song])]
-  return(similarSongList[:20])#changed k=11 to k=21 and [:10] to [:20]
+  return(similarSongList[:20])
